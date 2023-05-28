8736253c4c0ba11e9fee035e53c0d0b62235846b
k-chandravir <kumar.chandravir@m2pfintech.com>
2023-05-28 23:42:45 +0530

    Fix. Fixed the bug of updation of health of hero and villain after getting hit

--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -6,7 +6,7 @@ function Game() {
     // functonalities related to hero
     const [heroHealth, setHeroHealth] = useState();
     
-    const fetchHeroHealth = async() => {
+    const fetchHeroHealth = async () => {
         await axios.get("http://localhost:8080/hero-health")
             .then((response) => {
                 console.log(response);
@@ -14,13 +14,29 @@ function Game() {
             });
     }
 
+    const heroTakesDamage = async () => {
+        await axios.post("http://localhost:8080/shoot?hero-or-villain=hero")
+            .then((response) => {
+                console.log(response.data);
+                fetchHeroHealth();
+            })
+    }
+
     // functionalities related to villain
     const [villainHealth, setVillainHealth] = useState();
 
-    const fetchVillainHealth = async() => {
+    const fetchVillainHealth = async () => {
         await axios.get("http://localhost:8080/villain-health")
             .then((response) => {
                 setVillainHealth(response.data);
+            })
+    }
+
+    const villainTakesDamage = async () => {
+        await axios.post("http://localhost:8080/shoot?hero-or-villain=villain")
+            .then((response) => {
+                console.log(response.data);
+                fetchVillainHealth();
             })
     }
 
@@ -30,6 +46,15 @@ function Game() {
         fetchVillainHealth();
     }, [])
 
+    const heroAttacks = () => {
+        villainTakesDamage();
+        // fetchVillainHealth();
+    }
+
+    const villainAttacks = () => {
+        heroTakesDamage();
+        // fetchHeroHealth();
+    }
     // const fetchHeroHealth = () => {
     //     axios.get("http://localhost:8080/hero-health")
     //             .then(response => {
@@ -51,9 +76,9 @@ function Game() {
   return (
     <div>
         <div data-testid="heroHealth"> {heroHealth} </div>
-        <button>Click To Attack The Villain</button>
+        <button onClick={ heroAttacks }>Click To Attack The Villain</button>
         <div data-testid="villainHealth"> {villainHealth} </div>
-        <button>Click To Attack The Hero</button>
+        <button onClick={ villainAttacks }>Click To Attack The Hero</button>
         <button>Turn On Villain's Armour</button>
     </div>
   )
